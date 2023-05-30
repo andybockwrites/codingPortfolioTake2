46988bbba118dc8263b41ddcfcaa55700779a952
Andy Bock <andybockwrites@gmail.com>
2023-05-30 14:58:22 -0400

    working with chat bcs

--- reactions/src/index.js
+++ reactions/src/index.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { StrictMode } from 'react';
+import { render } from 'react-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import App from './App';
+
+const rootElement = document.getElementById('root');
+render(
+    <StrictMode>
+        <App />
+    </StrictMode>,
+    rootElement
 );
 
 // If you want to start measuring performance in your app, pass a function
